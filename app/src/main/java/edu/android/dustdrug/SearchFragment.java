28104996cb0de7e239a03fde5315b5015104607e
suhyeoney <sooheyong102@gmail.com>
2018-04-26 15:35:17 +0900

    Spinner 처리

--- app/src/main/java/edu/android/dustdrug/SearchFragment.java
+++ app/src/main/java/edu/android/dustdrug/SearchFragment.java
@@ -1,21 +1,25 @@
 package edu.android.dustdrug;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import static edu.android.dustdrug.MainActivity.TAG;
 
 public class SearchFragment extends Fragment {
 
     private Spinner spinner;
     private Spinner spinner1;
+    private String sido; // 서울(특별시), 광역시, 전국8도
+    private String sigu; // i) 서울, 광역시에 각각 배속된 구 ii)도에 배속된 중소도시
 
     public SearchFragment() {
         // Required empty public constructor
@@ -37,8 +41,61 @@ public class SearchFragment extends Fragment {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.sido_array, android.R.layout.simple_spinner_item);
         spinner.setPrompt("시.도를 선택하십시오.");
         spinner.setAdapter(new NothingSelectedSpinnerAdapter(
-                adapter, R.layout.contact_spinner_row_nothing_selected, getContext()
+                adapter, R.layout.sido_spinner_row_nothing_selected, getContext()
         ));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                sido = ((TextView) view).getText().toString();
+                Log.i(TAG, "sido : " + sido);
+                if(sido.equals("서울")) {
+                    ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(), R.array.seoul_gu_array, android.R.layout.simple_spinner_item);
+                    spinner1.setPrompt("구를 선택하십시오.");
+                    spinner1.setAdapter(new NothingSelectedSpinnerAdapter(
+                            adapter1, R.layout.sigu_spinner_row_nothing_selected, getContext()
+                    ));
+
+                    spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            sigu = ((TextView) view).getText().toString();
+                            Log.i(TAG, "sigu : " + sigu);
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                        }
+                    });
+                }
+
+                else if(sido.equals("광주")) {
+                    ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(), R.array.gwangju_gu_array, android.R.layout.simple_spinner_item);
+                    spinner1.setPrompt("구를 선택하십시오.");
+                    spinner1.setAdapter(new NothingSelectedSpinnerAdapter(
+                            adapter1, R.layout.sigu_spinner_row_nothing_selected, getContext()
+                    ));
+
+                    spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            sigu = ((TextView) view).getText().toString();
+                            Log.i(TAG, "sigu : " + sigu);
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
         return view;
     }
 
@@ -51,5 +108,45 @@ public class SearchFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+//    public void showGuSpinner(int textArrayResId) {  // 해당 string-array ID를 파라미터로 받아 고유의 스피너 생성하는 메소드 (구)
+//        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(), textArrayResId, android.R.layout.simple_spinner_item);
+//        spinner1.setPrompt("구를 선택하십시오.");
+//        spinner1.setAdapter(new NothingSelectedSpinnerAdapter(
+//                adapter1, R.layout.sido_spinner_row_nothing_selected, getContext()
+//        ));
+//
+//        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                sigu = ((TextView) view).getText().toString();
+//                Log.i(TAG, "sigu : " + sigu);
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//            }
+//        });
+//    }
+
+//    public void showSiSpinner(int textArrayResId) {  // 해당 string-array ID를 파라미터로 받아 고유의 스피너 생성하는 메소드 (전국8도에 각각 배속된 중소도시)
+//        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(), textArrayResId, android.R.layout.simple_spinner_item);
+//        spinner1.setPrompt("시를 선택하십시오.");
+//        spinner1.setAdapter(new NothingSelectedSpinnerAdapter(
+//                adapter1, R.layout.sido_spinner_row_nothing_selected, getContext()
+//        ));
+//
+//        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                sigu = ((TextView) view).getText().toString();
+//                Log.i(TAG, "sigu : " + sigu);
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//            }
+//        });
+//    }
 
 }
